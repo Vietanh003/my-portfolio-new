3c4a3673f2e6aa392fca6db83c776391458648fc
VietAnh <vanhdz246@gmail.com>
2025-11-03 17:17:24 +0700

    up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/Header.module.css";
 import logo from "../assets/images/logo.png";
 import { Link } from "react-router-dom";
+import { Menu, X } from "lucide-react"; // cài: npm install lucide-react
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.navContainer}>
         {/* Logo */}
-        <Link to="/" className={styles.logo}>
+        <Link to="/" className={styles.logo} onClick={closeMenu}>
           <img src={logo} alt="Logo" />
         </Link>
 
+        {/* Hamburger icon for mobile */}
+        <button className={styles.menuToggle} onClick={toggleMenu}>
+          {menuOpen ? <X size={28} /> : <Menu size={28} />}
+        </button>
+
         {/* Navigation */}
-        <nav className={styles.nav}>
+        <nav className={`${styles.nav} ${menuOpen ? styles.open : ""}`}>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/" onClick={closeMenu}>Home</Link>
             </li>
             <li>
-              <Link to="/about">Info</Link>
+              <Link to="/about" onClick={closeMenu}>Info</Link>
             </li>
             <li>
-              <Link to="/projects">Projects</Link>
+              <Link to="/projects" onClick={closeMenu}>Projects</Link>
             </li>
             <li>
-              <Link to="/contact">Contact</Link>
+              <Link to="/contact" onClick={closeMenu}>Contact</Link>
             </li>
           </ul>
         </nav>
